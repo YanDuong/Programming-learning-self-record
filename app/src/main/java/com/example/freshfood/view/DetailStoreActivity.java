7736551c4cc4766e7a24d7306dfbaa45ec964c02
YanDuong <aerithstrike@gmail.com>
2020-08-18 00:02:08 +0700

    'save'

---
+++ app/src/main/java/com/example/freshfood/view/DetailStoreActivity.java
@@ -0,0 +1,14 @@
+package com.example.freshfood.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailStoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_store);
+    }
+}
